69ef5be6d139704b9efc5f545976a53699ebaf91
JohlT <126289080+JohlT@users.noreply.github.com>
2023-03-16 22:11:01 -0400

    updated file path in functions to reflect new file folder in the project folder.

--- App1/functions.py
+++ App1/functions.py
@@ -1,12 +1,12 @@
 def get_To_Do(local_read='ToDo.txt'):
     """Reads to-do list file and grabs the contents"""
-    with open(f'../Files/{local_read}', 'r') as file_local:
+    with open(f'files/{local_read}', 'r') as file_local:
         td_list = file_local.readlines() 
     return td_list
 
 
 def store_To_Do(td_arg, local_write='ToDo.txt'):
     """writes adjusted list to the to-do text file."""
-    with open(f'../Files/{local_write}', 'w') as file:
+    with open(f'files/{local_write}', 'w') as file:
         file.writelines(td_arg)
 
